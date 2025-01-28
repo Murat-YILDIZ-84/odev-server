01a8d8752b5594dbc6f38ee950eea61fb4aa920b
Murat YILDIZ <test2@gmail.com>
2025-01-28 20:20:25 +0300

    Update - 20-2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require("express");
-const cors = require('cors');
+//const cors = require('cors');
 const app = express();
 const { Pool } = require('pg');
 const pool = new Pool({
@@ -15,16 +15,16 @@ var token = 0;
 const username = "apitest";
 const password = "test123";
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
     next();
-});
+});*/
 
 pool.connect((err) => {
     try {
@@ -35,7 +35,7 @@ pool.connect((err) => {
 })
 
 app.get("/test", (req, res) => {
-    res.send("Hello World-20");
+    res.send("Hello World-20-2");
 })
 
 app.get("/tables", async (req, res) => {
