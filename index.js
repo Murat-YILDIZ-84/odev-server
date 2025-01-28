5ac93bf35fdc523d71e28a12920af3ee0a2607ac
Murat YILDIZ <test2@gmail.com>
2025-01-28 18:49:01 +0300

    Update - 18-2

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ pool.connect((err) => {
     }
 })
 
+app.get('/cors', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send({ "msg": "This has CORS enabled 🎈" })
+});
+
 app.get("/test", (req, res) => {
     res.send("Hello World-18");
 })
