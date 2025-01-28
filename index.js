0e800cf4ef54021424c02131cce69ca0b1837e44
Murat YILDIZ <test2@gmail.com>
2025-01-29 01:26:07 +0300

    Update - 20-15

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.post("/token", async (req, res) => {
             token = d.getTime();
             res.json(
             {
-                "response": { "token": _base64 },
+                "response": { "token": req.header },
                 "messages": [{ "code": "0", "message": "OK" }]
             });
         /*} else {
