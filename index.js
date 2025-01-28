8971888b3f570111c9b0a20bad48cc135f7afaca
Murat YILDIZ <test2@gmail.com>
2025-01-28 04:48:15 +0300

    Update - 16-7

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ const password = "test123";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 pool.connect((err) => {
     try {
