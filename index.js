25786b81245d7b3b74c667f19862cfa5a31667b2
Murat YILDIZ <test2@gmail.com>
2025-01-29 19:55:51 +0300

    Update - 20-51

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.post("/token", async (req, res) => {
 
         //
         try {
-            if (_base64 == "") { _base64 = req; }
+            if (_base64 == "") { _base64 = req.data; }
         } catch(e){}
         
 
