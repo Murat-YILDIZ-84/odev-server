e1fcef7466951bea3ca07ee17a3610ba155d21b2
Murat YILDIZ <test2@gmail.com>
2025-01-30 00:38:07 +0300

    Update - 20-57

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.get("/dropTable", async (req, res) => {
 
 app.post("/token", async (req, res) => {
 
-    res.send(req);
+    res.send(req.headers);
 
     //console.log(req.headers.authorization);
     //console.log(req.headers.origin);
