e630e8bd392669391d6ac42ed57735a12903b878
Murat YILDIZ <test2@gmail.com>
2025-01-30 19:49:49 +0300

    Update - 21-3

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ app.patch("/getData", async (req, res) => {
             if (_token == "") { _token = req.body.authorization.split(" ")[1]; }
         } catch(e){}
 
-        console.log(_token);
+        console.log(req.body);
         
         /*const _script = req.body.script;
 
