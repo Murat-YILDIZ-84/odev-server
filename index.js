42e45e11347f4a692320180903f9fbe38abef64a
Murat YILDIZ <test2@gmail.com>
2025-02-04 21:58:07 +0300

    commit - 22-4

--- index.js
+++ index.js
@@ -38,10 +38,8 @@ app.get("/test", (req, res) => {
     /*res.send("Hello World");
     res.end();*/
 
-    res.writeHead(200, {
-    'Content-Type': 'text/json'
-    });
-    res.write(JSON.stringify(people));
+    res.writeHead(200, {'Content-Type': 'text/json'});
+    res.write("Hello World");
     res.end();
 })
 
